1b1e39889afabec98d69176617c41ce8ee8438b5
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-01 14:25:08 +0300

    Lisätty HTML5-esimerkkejä.

---
+++ HTML/CanvasDemo.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("piirtoalue");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "purple";
+ctx.fillRect(10, 10, 150, 100);
+
+ctx.fillStyle = "green";
+ctx.fillRect(200, 200, 300, 200);
