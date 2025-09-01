7b8b5855cea5bd81e51565a4775228aa28ddb6bb
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-01 15:41:50 +0300

    Lisätty kaksi JavaScript-esimerkkiä, eli nuolifunktiot sekä mallimerkkijonot.

---
+++ JavaScript/Funktiot.js
@@ -0,0 +1,31 @@
+function laskeSumma(a, b) {
+    return a + b;
+}
+
+const tulos = laskeSumma(5, 10);
+console.log(tulos);
+
+// -----------------------
+
+const laskentaFunktio = function (a, b) {
+    return a + b;
+};
+
+const tulos2 = laskentaFunktio(5, 10);
+console.log(tulos2);
+
+// -----------------------
+
+const nuoliFunktio = (a, b) => {
+    return a + b;
+};
+
+const tulos3 = nuoliFunktio(5, 10);
+console.log(tulos3);
+
+// -----------------------
+
+const nuoliFunktio2 = (a, b) => a + b;
+
+const tulos4 = nuoliFunktio2(5, 10);
+console.log(tulos4);
---
+++ JavaScript/Merkkijonot.js
@@ -0,0 +1,8 @@
+const x = 10;
+const y = 20;
+
+const tuloste = "X:n arvo on " + x + " ja Y:n arvo on " + y;
+console.log(tuloste);
+
+const tuloste2 = `X:n arvo on ${x} ja Y:n arvo on ${y}`;
+console.log(tuloste2);
