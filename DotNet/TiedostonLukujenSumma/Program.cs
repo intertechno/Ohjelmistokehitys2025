7909c09d62b43c4f91d9454a1c9be4cae18bd58f
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-16 11:53:47 +0300

    Lisätty C#-esimerkki tiedostojen lukemisesta ja lukujen summan laskennasta.

---
+++ DotNet/TiedostonLukujenSumma/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Aloitetaan luvut-tiedoston käsittely.");
+
+string[] rivit = File.ReadAllLines("..\\Luvut.txt");
+Console.WriteLine("Tiedoston rivit luettu.");
+
+int summa = 0;
+foreach (string rivi in rivit)
+{
+    // Console.WriteLine(rivi);
+    int luku = int.Parse(rivi);
+    summa += luku;
+}
+
+// tulostus
+Console.WriteLine($"Lukujen summa on {summa}.");
+float keskiarvo = (float)summa / rivit.Length;
+Console.WriteLine($"Lukujen keskiarvo on {keskiarvo}.");
+
+Console.WriteLine("Lopetetaan luvut-tiedoston käsittely.");
