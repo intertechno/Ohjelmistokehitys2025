36cb6295706b9d3a48ee8f19fc04d3233da71086
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-16 15:21:43 +0300

    Lisätty C#-esimerkki merkkijonon käsittelystä ja hitaasta suorituskyvystä.

---
+++ DotNet/Merkkijonot/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Merkkijonon käsittely alkaa...");
+
+DateTime alku = DateTime.Now;
+string s = "";
+for (int i = 0; i < 600_000; i++)
+{
+    s = s + "A";
+}
+DateTime loppu = DateTime.Now;
+
+// tulokset
+TimeSpan kesto = loppu - alku;
+Console.WriteLine($"Merkkijonon käsittely on päättynyt. Kesto: {kesto.TotalMilliseconds} ms");
