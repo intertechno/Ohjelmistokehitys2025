f923592dd53a13bc7df33407ae1bcc8bff3aeabb
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-18 11:37:10 +0300

    Lisätty uusi API-kontrolleri esimerkkinä reitityksistä.

---
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/TestiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/testi")]
+    public class TestiController : ControllerBase
+    {
+        public string EkaTesti()
+        {
+            return "Moi maailma!";
+        }
+    }
+}
