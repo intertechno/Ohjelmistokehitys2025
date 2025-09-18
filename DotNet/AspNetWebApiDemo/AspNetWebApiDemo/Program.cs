4d4e65b8d995594d3cce2ca6e8a0a353fa9c1587
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-18 15:52:13 +0300

    Lisätty ASP.NET Web API:iin CORS-määritykset React-sovellusta varten.

--- DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Program.cs
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Program.cs
@@ -1,4 +1,16 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// CORS-määritykset React-fronttia varten
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
 
 // Add services to the container.
 
@@ -15,6 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
