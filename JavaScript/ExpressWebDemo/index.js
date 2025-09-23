8ca7b2ada09b64e027ba6695bd0b21666b698bae
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-23 13:23:00 +0300

    Lisätty Express-sovellukseen uusi API-toiminto joka lukee CSV-tiedoston levyltä ja palauttaa sen tiedot JSON-muodossa.

--- JavaScript/ExpressWebDemo/index.js
+++ JavaScript/ExpressWebDemo/index.js
@@ -1,15 +1,29 @@
+const csv = require('csv-parser')
 const express = require('express');
+const fs = require('fs')
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><p>Welcome to my Express app!</p>');
+    res.send('<h1>Hello World!</h1><p>Welcome to my Express app!</p>');
 });
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, JSON!' });
+    res.json({ message: 'Hello, JSON!' });
+});
+
+app.get('/api/henkilot', (req, res) => {
+    const results = [];
+
+    fs.createReadStream('Henkilöt.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            res.json(results);
+        });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
+    console.log(`Example app listening on http://localhost:${port}`);
 });
