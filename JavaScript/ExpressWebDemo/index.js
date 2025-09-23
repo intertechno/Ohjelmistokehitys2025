21f1a68e591992110ae4853acec4bd83455fab9a
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-23 10:53:49 +0300

    Lisätty uusi API-osoite joka palauttaa yksinkertaisen JSON-olion.

--- JavaScript/ExpressWebDemo/index.js
+++ JavaScript/ExpressWebDemo/index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1><p>Welcome to my Express app!</p>');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, JSON!' });
 });
 
 app.listen(port, () => {
